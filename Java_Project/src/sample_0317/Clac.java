ec4c0873ca86ad0ff373e94276c6af9fce9148c4
basic <basic@DFC4FGY1>
2016-03-17 19:25:20 +0900

    오늘 처음 함

---
+++ Java_Project/src/sample_0317/Clac.java
@@ -0,0 +1,32 @@
+package sample_0317;
+
+import java.util.*; 
+
+public class Clac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5, y=9, result;
+		result = x % y;
+		System.out.println(result);
+
+		x = 2;
+		y= 2;
+		
+		result = x << y;
+		System.out.println(result);
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("a ют╥б го╩О");
+		int a = input.nextInt();
+		System.out.println("b ют╥б го╩О");
+		int b = input.nextInt();
+
+		result = a* b;
+		System.out.println(result);
+
+		input.close();
+		
+	}
+
+}
